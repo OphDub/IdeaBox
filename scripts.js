2b279636f29c657663165064d3baa103746e7e5a
Ophus Wong <ophus.wong@gmail.com>
2017-10-21 15:03:12 -0600

    Card functionality for add and delete

--- scripts.js
+++ scripts.js
@@ -1,7 +1,29 @@
 //Global Variables
-
+var ideaTitle = document.getElementById('idea-title-input');
+var ideaBody = document.getElementById('idea-body');
+var saveIdeaButton = document.querySelector('.save-button');
+var deleteIdea = document.querySelector('.idea-delete');
 
 //Event Listeners
-
+saveIdeaButton.addEventListener('click', appendIdeaCard);
 
 //Functions
+function appendIdeaCard () {
+	var ideaWrapper = document.querySelector('.idea-wrapper');	
+	var ideaCard = document.createElement('article');
+	ideaCard.classList.add('idea-card');
+	ideaCard.innerHTML = 
+	`<h2 class="idea-title-h2"> ${ideaTitle.value} <button type="button" class="idea-delete"></button></h2>
+		<p class="idea-body-text"> ${ideaBody.value} </p>
+		<button class="upvote voting-buttons" type="button"></button>
+		<button class="downvote voting-buttons" type="button"></button>
+		<p class="idea-quality"> quality: <span class="idea-status">swill</span></p>`
+	ideaWrapper.prepend(ideaCard);
+	document.querySelector('form').reset();
+}
+
+$(document).on('click', function(){
+	$('.idea-delete').click(function() {
+	$(this).closest('article').remove();
+	});
+});
