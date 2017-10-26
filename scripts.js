867050da25d2fd3eb9e00ecd23032e742e7e2bdd
Ricardo Viera <ricardoclean@gmail.com>
2017-10-25 20:13:32 -0600

    Functionality for editing cards

--- scripts.js
+++ scripts.js
@@ -22,8 +22,25 @@ function saveIdea () {
 	var $stringifiedIdea = JSON.stringify($newIdea);
 	localStorage.setItem($ideaId, $stringifiedIdea);
 	retrieveIdea($ideaId);
-	
 };
+
+function saveBody (id, ideaBody) {
+	var retrieved = localStorage.getItem(id);
+	var parsed = JSON.parse(retrieved);
+	parsed.ideaBody = ideaBody;
+	var stringBody = JSON.stringify(parsed);
+	localStorage.setItem(id, stringBody);
+};
+
+function saveTitle (id, ideaTitle) {
+	var retrieved = localStorage.getItem(id);
+	var parsed = JSON.parse(retrieved);
+	parsed.ideaTitle = ideaTitle;
+	var stringTitle = JSON.stringify(parsed);
+	localStorage.setItem(id, stringTitle);
+};
+
+
 
 function retrieveIdea(e) {
 	var retrievedIdea = localStorage.getItem(e);
@@ -38,13 +55,48 @@ window.onload = function() {
 	}
 };
 
+//Change text on inputs
+
+$('.idea-wrapper').on('click', '.idea-title', function() {
+    $(this).attr('contenteditable','true').addClass('edit-title').focus();
+    var $editThis = $(this).closest('article').attr('id');
+
+    $(this).keyup(function() {
+    	var $newTitle = $(this).text();
+    	saveTitle ($editThis, $newTitle);
+		});
+
+		$(this).on('blur', function(){
+				$(this).removeClass('edit-title');
+		});
+});
+
+
+$('.idea-wrapper').on('click', '.idea-body-text', function() {
+    $(this).attr('contenteditable','true').addClass('edit-body').focus();
+    var editThis = $(this).closest('article').attr('id');
+    $(this).keyup(function() {
+    	var newBody = $(this).text();
+    	saveBody(editThis, newBody);
+		});
+
+		$(this).on('blur', function(){
+				$(this).removeClass('edit-body');
+		});
+});
+
+
+
+//Change text on inputs
+
 function displayIdeaCard (ideaTitle, ideaBody, ideaId) {
 	var ideaWrapper = document.querySelector('.idea-wrapper');	
 	var ideaCard = document.createElement('article');
 	ideaCard.classList.add('idea-card');
 	ideaCard.id = ideaId;
 	ideaCard.innerHTML = 
-		`<h2 class="idea-title-h2"> ${ideaTitle} </h2><button type="button" class="idea-delete"></button></h2>
+		`<h2 class="idea-title"> ${ideaTitle} </h2>
+		<button type="button" class="idea-delete"></button>
 		<p class="idea-body-text"> ${ideaBody} </p>
 		<button class="upvote voting-buttons" type="button"></button>
 		<button class="downvote voting-buttons" type="button"></button>
@@ -86,5 +138,4 @@ function upVote () {
 
 	});
 	//If Conditional to change ideaQuality value of the object
-	
 };	
